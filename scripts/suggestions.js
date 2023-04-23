02509edc725dfb3722a3be4a72f48c90c512c87e
Lucas Paduam <79526125+LucasEPaduam@users.noreply.github.com>
2023-04-23 15:40:14 -0300

    Add files via upload

---
+++ scripts/suggestions.js
@@ -0,0 +1,1 @@
+let homenageados = ["Andressa de Souza Ugaya", "Carmen da Silva Ferreira", "Elisa Lucas Rodrigues", "Hélio dos Santos", "Idenilde de Almeida Conceição", "Julio Ludemir", "Luana Genot", "Marcivan Barreto", "Salloma Salomão", "Sivaldo Camargo"]
